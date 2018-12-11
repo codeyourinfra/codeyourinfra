da84960f56b8079d0e56f34f1b7ffb6e23f059ff
Gustavo Muniz do Carmo <gustavo@codeyourinfra.today>
2018-12-11 19:05:25 +0000

    ubuntu upgrade and test fix

--- same_cfgfile_diff_content/test.sh
+++ same_cfgfile_diff_content/test.sh
@@ -4,7 +4,7 @@ tmpfile=$(mktemp)
 teardown()
 {
 	vagrant destroy -f
-	rm -rf .vagrant/ *.retry "$tmpfile" ubuntu-xenial-16.04-cloudimg-console.log
+	rm -rf .vagrant/ *.retry "$tmpfile" ubuntu-*-cloudimg-console.log
 }
 
 . ../common/test-library.sh
@@ -17,13 +17,13 @@ checkPlaybookSyntax playbook.yml hosts
 
 # execute the solution
 ansible-playbook playbook.yml -i hosts | tee ${tmpfile}
-assertEquals 3 $(tail -5 ${tmpfile} | grep -c "failed=0")
+assertEquals 3 $(tail -10 ${tmpfile} | grep -c "failed=0")
 
 # validate the solution
 ansible qa -i hosts -m shell -a "cat /etc/conf" | tee ${tmpfile}
-assertEquals "prop1=Aprop2=B" $(awk '/qa1/ {for(i=1; i<=2; i++) {getline; printf}}' ${tmpfile})
-assertEquals "prop1=Cprop2=D" $(awk '/qa2/ {for(i=1; i<=2; i++) {getline; printf}}' ${tmpfile})
-assertEquals "prop1=Eprop2=F" $(awk '/qa3/ {for(i=1; i<=2; i++) {getline; printf}}' ${tmpfile})
+assertEquals "prop1=Aprop2=B" $(awk '/qa1/ {for(i=1; i<=2; i++) {getline; printf $0}}' ${tmpfile})
+assertEquals "prop1=Cprop2=D" $(awk '/qa2/ {for(i=1; i<=2; i++) {getline; printf $0}}' ${tmpfile})
+assertEquals "prop1=Eprop2=F" $(awk '/qa3/ {for(i=1; i<=2; i++) {getline; printf $0}}' ${tmpfile})
 
 # turn off the environment and exit
 teardown
