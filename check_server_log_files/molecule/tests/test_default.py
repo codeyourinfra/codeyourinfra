41deff40a9d82cb2288c8817b260f3bd073bbe2c
Gustavo Muniz do Carmo <gustavo@codeyourinfra.today>
2019-06-25 16:04:52 +0100

    replace oracle jdk with openjdk

--- check_server_log_files/molecule/tests/test_default.py
+++ check_server_log_files/molecule/tests/test_default.py
@@ -6,10 +6,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_oracle_java8_is_installed(host):
-    oracle_java8 = host.package("oracle-java8-installer")
-    assert oracle_java8.is_installed
-    assert oracle_java8.version.startswith("8u")
+def test_java8_is_installed(host):
+    java8 = host.package("openjdk-8-jdk")
+    assert java8.is_installed
+    assert java8.version.startswith("8u")
 
 
 def test_jenkins_is_installed(host):
